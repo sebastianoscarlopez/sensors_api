1575e57bf0001f26f360db6b14b6647b4494fde5
Sebastian Oscar Lopez <sebastian.oscar.lopez@gmail.com>
2021-02-03 14:03:16 -0500

    inserta, consulta y reinicia las mediciones

---
+++ data.js
@@ -0,0 +1,35 @@
+import mongodb from 'mongodb/index.js'
+const { MongoClient } = mongodb
+
+const connect = async (action) => {
+	const client = new MongoClient(process.env.CONNECTIONSTRING, { useUnifiedTopology: true, useNewUrlParser: true })
+	client.connect(async (err) => {
+		// perform actions on the collection object
+		await action(client)
+		client.close()
+	})
+}
+
+
+export const measurements = {
+	insert: ({ sensorid, time, value }) => {
+		connect(async (client) => {
+			const collection = client.db("sensors").collection("measurements")
+			collection.insertOne({ sensorid, time, value })
+		})
+	},
+	get: async () => {
+		return new Promise((resolve, reject) => {	
+			connect(async (client) => {
+				const collection = client.db("sensors").collection("measurements")
+				collection.find().toArray().then(resolve)
+			})
+		})
+	},
+	reset: () => {
+		connect(async (client) => {
+			const collection = client.db("sensors").collection("measurements")
+			collection.deleteMany({ })
+		})
+	},
+}
---
+++ index.js
@@ -0,0 +1,31 @@
+import dotenv from 'dotenv'
+import express from 'express'
+import { measurements} from './data.js'
+
+dotenv.config()
+
+const app = express()
+
+app.get('/', (req, res) => {
+	res.send('sensors api')
+})
+
+app.get('/measurements', async (req, res) => {
+	const { sensorid, time, value } = req.query
+	const values = await measurements.get()
+	res.send(values)
+})
+
+app.get('/save', (req, res) => {
+	const { sensorid, time, value } = req.query
+	const id = measurements.insert({ sensorid, time, value })
+	res.send('')
+})
+
+app.get('/reset', (req, res) => {
+	measurements.reset()
+	res.send('')
+})
+
+app.listen(process.env.PORT || 3000)
+
